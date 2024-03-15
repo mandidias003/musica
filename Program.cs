f1d76d693549b85617f26822ffe2cc052ab804f5
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-15 20:08:55 -0300

    acabei

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Um minuto para o fim do mundo");
+
+
+Console.ResetColor();
+
+Console.WriteLine(@"
+Me sinto só
+Mas quem é que nunca se sentiu assim?
+Procurando um caminho pra seguir uma direção
+Respostas!
+Um minuto para o fim do mundo
+Toda sua vida em 60 segundos
+Uma volta no ponteiro do relógio pra viver
+
+O tempo corre contra mim
+Sempre foi assim e sempre vai ser
+Vivendo apenas pra vencer a falta que me faz você
+De olhos fechados eu tento esconder a dor agora
+Por favor entenda eu preciso ir embora porque");
+
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine(@"
+Quando estou com você
+Sinto meu mundo acabar
+Perco o chão sob os meus pés
+Me falta o ar pra respirar
+E só de pensar em te perder por um segundo
+Eu sei que isso é o fim do mundo
+");
+Console.ResetColor();
+
+Console.WriteLine(@"
+Volto o relógio para trás tentando adiar o fim
+Tentando esconder o medo de te perder quando me sinto assim
+De olhos fechados eu tento enganar meu coração
+Fugir pra outro lugar em uma outra direção porque");
+
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine(@"
+Quando estou com você
+Sinto meu mundo acabar
+Perco o chão sob os meus pés
+Me falta o ar pra respirar
+E só de pensar em te perder por um segundo
+Eu sei que isso é o fim do mundo
+
+Quando estou com você
+Sinto meu mundo acabar
+Perco o chão sob os meus pés
+Me falta o ar pra respirar
+E só de pensar em te perder por um segundo
+Eu sei que isso é o fim do mundo
+");
+Console.ResetColor();
+
+Console.WriteLine(@"
+Eu sei que isso é o fim do mundo
+
+Eu sei que isso é o fim
+Eu sei que isso é o fim");
